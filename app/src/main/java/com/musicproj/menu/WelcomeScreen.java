677defe309917c920bef1ee0ea955c3c6b415e06
asimas12 <asimas12@ufl.edu>
2017-08-06 18:53:02 -0400

    inital commit

---
+++ app/src/main/java/com/musicproj/menu/WelcomeScreen.java
@@ -0,0 +1,32 @@
+package com.musicproj.menu;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+
+
+
+
+
+    }
+}
